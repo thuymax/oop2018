9b1f3568cf7f9c84dff73bd486e303de6a311147
phamtrungthuy <43264965+thuymax@users.noreply.github.com>
2018-10-04 20:46:33 +0700

    Update Week3.java

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -4,12 +4,17 @@ public class Week3 {
 
     public static int max(int m, int n) {
         // TODO: Tìm giá trị lớn nhất của hai số nguyên, giá trị trả về của hàm là số lớn nhất
-        return -1;
+        if(n>m) return n;
+       else return m;
     }
 
     public static int minOfArray(int[] array) {
         // TODO: Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
-        return -1;
+        int min=array[0];
+        for(int i=1;i<array.length;i++){
+            if(array[i]<min) min=array[i];
+        }
+        return min;
     }
 
     /**
@@ -20,6 +25,11 @@ public class Week3 {
      */
     public static String calculateBMI(double weight, double height) {
         // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
-        return null;
+        double BMI;
+        BMI=weight/(height*height);
+        if(BMI<18.5) return "Thiếu cân";
+        else if(BMI>=18.5&&BMI<22.99) return "Bình Thường";
+        else if(BMI>=23&&BMI<24.99) return "Thừa cân";
+        else return "Béo phì";
     }
 }
