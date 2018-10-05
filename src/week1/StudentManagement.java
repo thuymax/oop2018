b9895ef6c6f52d05fce1420a260c3639bc733af6
phamtrungthuy <43264965+thuymax@users.noreply.github.com>
2018-10-05 15:11:57 +0700

    Update StudentManagement.java

--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -1,101 +1,91 @@
 package week1;
-import week1.Student;
 
 public class StudentManagement {
-
+    private Student[] students = new Student[100];
+    private int size_of_students = 0;
+    private int classSize = 0;
+    private String[] classList = new String[100];
+    
+    
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
-        static Student student[];
+
     public boolean sameGroup(Student s1, Student s2) {
-        // TODO
-        if(s1.getGroup().equals(s2.getGroup())
-            return true;
-        else return false;
-        //return false; // xóa dòng này sau khi cài đặt
+        // TODO:
+        
+        return s1.getGroup().equals(s2.getGroup()) ;
     }
+    public void addStudent(Student a){
+        if(size_of_students <= 100){
+            students[size_of_students] = a;
+            size_of_students ++;
+        }
+        else{
+            System.out.println("FULL");
+        }
+        
+    }
+    
+    
 
     void studentsByGroup() {
-        // TODO:
-        int n = student.length; 
-    	String str = "";
-    	int a[] = new int [100];
-    	
-    	for(int i = 0; i < a.length; i++) a[i] = 0;
-    	for(int i = 0; i < n; i++) {
-    		if(a[i] == 0) {
-    			str = student[i].getGroup();
-    			System.out.println("the class of student " + str + " is :");
-    			for(int j = 0; j < n ; j++) {
-                            if(a[j] == 0 && student[j].getGroup().equals(str)) {
-    				a[j] = 1;
-    				System.out.println( student[j].getName() + " ");
-    				}
-    			}
-    		}
-    	}
+        for(int i= 0; i< size_of_students; i++){
+            boolean check = true;
+            for(int j=0; j < classSize; j++){
+                if(students[i].getGroup().equals(classList[j])){
+                    check = false;
+                }
+            }
+            if(check){
+                classList[classSize] = students[i].getGroup();
+                classSize++ ;
+            }
+        }
+        for(int i=0; i<classSize; i++){
+            System.out.println("LỚP: " + classList[i]);
+            for(int j=0; j < size_of_students ; j++){
+                if(students[j].getGroup().equals(classList[i])){
+                    System.out.println(students[j].getInfo());
+                    System.out.println("----------");
+                }
+            }
+            System.out.println("*********************************");
+        }
+        
+        
     }
 
     void removeStudent(String id) {
-        // TODO:
-        int location = 0;
-    	  int n = student.length;
-   	      while(location < n) {
-   	    	  if(student[location].getId().equals(id)) {
-   	    		  for(int i = location; i < n ; i++) {
-   	    			  student[i] = student[i + 1];
-   	    			  location = n;
-   	    			  n--;
-   	    		  }
-   	    	  }
-   	    	  else {
-   	    		  location++;
-   	    	  }
-   	      }
+        for(int i=0; i < size_of_students; i++){
+            if(students[i].getId().equals(id)){
+                for(int j = i; j < size_of_students ; j++){
+                    students[j] = students[j+1];
+                }
+                size_of_students--;
+                break;
+            }
+        }
     }
+    
 
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-  		Student thuy = new Student();
-  		thuy.setName("Thuy");
-  		thuy.setId("17021051");
-  		thuy.setGroup("INT22041");
-  		thuy.setEmail("trungthuy99x@gmail.com");
-  		thuy.getInfo();
-    	
-  		// cehck constructor 1
-  		Student thuy = new Student();
-  		thuy.getInfo();
-  		
-    	/*check constructor 2 */
-    	Student thuy = new Student("Thuy", "17021051", "phamtrungthuy99xx");
-    	thuy.getInfo();
-    	
-    	/* check constructor 3 */
-    	Student thuy = new Student();
-    	Student copy = new Student(thuy);
-    	copy.getInfo();
-    	Student thuy = new Student("Thuy", "17021051", "phamtrungthuy", "INT22041");
-    	Student thinh = new Student("Thinh", "17021045","nguyendinhthinh", "INT22041");
-    	Student manh = new Student("Manh", "17029712", "nguyenducmanh", "INT22042");
-    	boolean kiemTra = sameGroup(thuy,manh );
-    	if(kiemTra == true) {
-    		System.out.println("same group");
-    	}
-    	else {
-    		System.out.println("not same group");}
-    	
-        student = new Student[100];
-    	for(int i = 0; i < 3; i++) {
-    		student[0] = thuy;
-    		student[1] = thinh;
-    		student[2] = manh;
-    	}
-    	for(int i = 0; i < 3; i++) {
-    		student[i].getInfo();
-        }
-    	removeStudent("17029712");
-    	for(int i = 0; i < student.length; i++) {
-    		student[i].getInfo();
-        }
-    	studentsByGroup();
-  	}
+        // TODO:
+        Student a = new Student("NGuyen Dinh Thinh", "17021045", "thinhnguyenxc@gmail.com");
+        Student b = new Student("Nguyen Van A", "17021046", "A@gmail.com");
+        Student c = new Student("Nguyen Van B", "17021046", "B@gmail.com");
+        c.setGroup("INT22042");
+        StudentManagement test = new StudentManagement();
+        if(test.sameGroup(a, b)) System.out.println("a va b cung lop");
+        else System.out.println("a va b khac lop");
+        StudentManagement temp = new StudentManagement();
+        temp.addStudent(a);
+        temp.addStudent(b);
+        temp.addStudent(c);
+        temp.studentsByGroup();
+        
+    }
     
 }
