26c61b912dd306ada43f5d604655eddb8182db7b
phamtrungthuy <43264965+thuymax@users.noreply.github.com>
2018-11-01 22:30:24 +0700

    Update Square.java

--- src/week7/task1/Square.java
+++ src/week7/task1/Square.java
@@ -1,4 +1,14 @@
 package week7.task1;
 
 public class Square {
+  private Expression expression;
+	public Square(Expression expression) {
+		this.expression=expression;
+	}
+	public int evaluate() {
+		return expression.evaluate();
+	}
+	public String toString() {
+		return "("+expression.toString()+")";
+	}
 }
