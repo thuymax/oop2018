f51133dfdfb65e98796dc0a1d9bfc0354d937c2c
phamtrungthuy <43264965+thuymax@users.noreply.github.com>
2018-11-01 22:29:53 +0700

    Update Addition.java

--- src/week7/task1/Addition.java
+++ src/week7/task1/Addition.java
@@ -1,4 +1,22 @@
 package week7.task1;
 
 public class Addition {
+  private Expression left;
+    private Expression right;
+	public Expression left() {
+		return this.left;
+	}
+	public Expression right() {
+		return this.right;
+	}
+	public Addition(Expression left, Expression right) {
+		this.left=left;
+		this.right=right;
+	}
+	public int evaluate(){
+        return left.evaluate()+right.evaluate();
+    }
+	public String toString() {
+        return "("+left.toString()+"+"+right.toString()+")";
+	}
 }
