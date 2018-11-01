be4c2c42d3e7506fba71924bdd3cefaa35780519
phamtrungthuy <43264965+thuymax@users.noreply.github.com>
2018-11-01 22:29:27 +0700

    Update Numeral.java

--- src/week7/task1/Numeral.java
+++ src/week7/task1/Numeral.java
@@ -1,4 +1,18 @@
 package week7.task1;
 
 public class Numeral {
+  private int x;
+	public Numeral(int x) {
+		this.x=x;
+	}
+	public Numeral() {
+		this.x=0;
+	}
+	public int evaluate() {
+		return this.x;
+	}
+	public String toString() {
+		return " "+ this.x;
+	}
+	
 }
