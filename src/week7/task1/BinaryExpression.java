2f59508c52222e85e87d5f38f277336e6157c8db
phamtrungthuy <43264965+thuymax@users.noreply.github.com>
2018-11-01 22:29:05 +0700

    Update BinaryExpression.java

--- src/week7/task1/BinaryExpression.java
+++ src/week7/task1/BinaryExpression.java
@@ -1,4 +1,8 @@
 package week7.task1;
 
 public class BinaryExpression {
+  public abstract int evaluate();
+	public abstract String toString();
+	public abstract Expression left();
+	public abstract Expression right();
 }
