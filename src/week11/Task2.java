e3d17414211413d445bfd9b86c01ae02feac473a
phamtrungthuy <43264965+thuymax@users.noreply.github.com>
2018-11-22 20:38:08 +0700

    Update Task2.java

--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,22 @@
 package week11;
-
+import java.util.ArrayList;
 public class Task2 {
+
+	public static<T extends Comparable> void max(ArrayList<T> L) {
+		T max=L.get(0);
+		for(int i=1;i<L.size();i++) {
+			if(L.get(i).compareTo(max)>0) {
+				max=L.get(i);
+			}
+		}
+		System.out.println("max= " + max);
+	}
+	public static void main(String[] thuy) {
+		ArrayList<Character> arr= new ArrayList<>();
+		arr.add('n');
+		arr.add('a');
+		arr.add('i');
+		arr.add('g');
+		max(arr);
+	}
 }
